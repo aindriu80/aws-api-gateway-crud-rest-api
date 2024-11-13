fec9af9fa9cae19e12b64f16ccff7880c3c18032
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2024-11-13 17:26:11 +0000

    Fix ESLint config for flat config system

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+/** @type {import('eslint').FlatConfig} */
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      globals: {
+        // Add global variables here if needed
+        browser: true,
+        es2021: true,
+      },
+    },
+    plugins: [],
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+    },
   },
-  extends: ["eslint:recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  rules: {
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "quotes": ["error", "single"],
-    "semi": ["error", "always"],
-  },
-};
-
+];
